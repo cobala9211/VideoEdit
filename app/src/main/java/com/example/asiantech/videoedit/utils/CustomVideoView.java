f9a38a16e26567aae5f1b03284c8b4938ad97462
TrangLT <TrangLT@asiantech.vn>
2016-10-25 08:24:47 +0700

    cut video with a period of time

---
+++ app/src/main/java/com/example/asiantech/videoedit/utils/CustomVideoView.java
@@ -0,0 +1,60 @@
+package com.example.asiantech.videoedit.utils;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.VideoView;
+
+import com.example.asiantech.videoedit.listeners.IPlayVideoListener;
+
+/**
+ * Copyright © 2016 AsianTech inc.
+ * Created by tranglt  on 21/10/2016.
+ */
+
+public class CustomVideoView extends VideoView {
+
+    private IPlayVideoListener mIPlayVideoListener;
+
+    public CustomVideoView(Context context) {
+        super(context);
+    }
+
+    public CustomVideoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomVideoView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public void setPlayPauseListener(IPlayVideoListener listener) {
+        mIPlayVideoListener = listener;
+    }
+
+
+    @Override
+    public void pause() {
+        super.pause();
+        if (mIPlayVideoListener != null) {
+            mIPlayVideoListener.onPause();
+        }
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        if (mIPlayVideoListener != null) {
+            mIPlayVideoListener.onPlay();
+        }
+    }
+
+    @Override
+    public void seekTo(int msec) {
+        super.seekTo(msec);
+        if (mIPlayVideoListener != null) {
+            mIPlayVideoListener.seekTo(msec);
+        }
+    }
+
+
+}
